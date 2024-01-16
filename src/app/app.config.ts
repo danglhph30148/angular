46e392e338f91d448a937f059d14d11bf13ec117
anhtucao <caoanhtu611@gmail.com>
2024-01-16 17:49:26 +0700

    up

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,16 +2,12 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
-import {
-  BrowserModule,
-  provideClientHydration,
-} from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideClientHydration(),
     provideHttpClient(),
     importProvidersFrom(BrowserModule),
     importProvidersFrom(BrowserAnimationsModule),
