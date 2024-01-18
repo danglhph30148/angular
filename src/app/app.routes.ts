89c70f4e5e979b29d3cd1fe6e241344a71bc2e82
danglhph30148 <danglhph30148@fpt.edu.vn>
2024-01-18 23:25:52 +0700

    upp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,15 @@ import { ProductsComponent } from './pages/admin/products/products.component';
 import { AdminComponent } from './layouts/admin/admin.component';
 import { CreateComponent } from './pages/admin/create/create.component';
 import { EditComponent } from './pages/admin/edit/edit.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'signup', component:SignupComponent },
+  { path: 'signin', component:SigninComponent },
+
   {
     path: 'admin',
     component: AdminComponent,
@@ -17,6 +22,7 @@ export const routes: Routes = [
       { path: 'products', component: ProductsComponent },
       { path: 'create', component: CreateComponent },
       { path: 'products/:id', component: EditComponent },
+      
     ],
   },
 ];
