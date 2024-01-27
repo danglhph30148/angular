e58045afdaea675adb2cc68152e1fcdac8382eaf
danglhph30148 <danglhph30148@fpt.edu.vn>
2024-01-27 20:11:35 +0700

    loginregisterr

---
+++ src/app/pages/admin/user/listuser/listuser.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listuser',
+  standalone: true,
+  imports: [],
+  templateUrl: './listuser.component.html',
+  styleUrl: './listuser.component.css'
+})
+export class ListuserComponent {
+
+}
